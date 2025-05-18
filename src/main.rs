fc4d4dd5161e13bd4339ddb7c70b634f2ea19a53
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2025-05-18 21:51:36 +0300

    refactor move and zoom, new colors

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::f64::consts::PI;
 
-use minifb::{Key, ScaleMode, Window, WindowOptions};
+use minifb::{Key, Window, WindowOptions};
 use num::{complex::{Complex64, ComplexFloat}, Zero};
 use rand::{rng, Rng};
 use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
@@ -37,7 +37,7 @@ fn main() {
 		let mut is_redraw_needed: bool = false;
 
 		(w, h) = window.get_size();
-		(wf, hf) = (wf, hf);
+		(wf, hf) = (w as float, h as float);
 		let new_size = w * h;
 		if new_size != buffer.len() {
 			buffer.resize(new_size, 0);
@@ -68,7 +68,7 @@ fn main() {
 		// Compute center world coords BEFORE zoom
 		let scx = w as float / 2.0; // screen center x
 		let scy = h as float / 2.0; // screen center x
-		let center_world_before = screen_to_world(scx, scy, w, h, zoom, cam_x, cam_y);
+		let center_world_before = screen_to_world(STW{x:scx, y:scy, wf, hf, zoom, cam_x, cam_y});
 
 		if window.is_key_down(Key::Z) || window.is_key_down(Key::I) {
 			zoom *= ZOOM_STEP;
@@ -84,7 +84,7 @@ fn main() {
 
 			// println!("zoom = {zoom}");
 			// Compute center world coords AFTER zoom
-			let center_world_after = screen_to_world(scx, scy, w, h, zoom, cam_x, cam_y);
+			let center_world_after = screen_to_world(STW{x:scx, y:scy, wf, hf, zoom, cam_x, cam_y});
 			// Adjust camera so center remains fixed
 			cam_x += center_world_before.0 - center_world_after.0;
 			cam_y += center_world_before.1 - center_world_after.1;
@@ -96,16 +96,15 @@ fn main() {
 				.for_each(|(i, pixel)| {
 					let x = (i % w) as float;
 					let y = (i / w) as float;
-					let (x, y) = screen_to_world(x as float, y as float, w, h, zoom, cam_x, cam_y);
-
+					let (x, y) = screen_to_world(STW{x, y, wf, hf, zoom, cam_x, cam_y});
 					let z_init = Complex64::new(x, y);
 					let mut z = z_init;
 					let mut z_last_not_nan = z;
 					let mut z_esc = Complex64::zero();
 					let mut is_bounded = true;
 					let mut escape_iter_n: u32 = 0;
-					const N_ITERS: u32 = 20;
-					for i in 0..N_ITERS {
+					let n_iters: u32 = 20;
+					for i in 0..n_iters {
 						if !z.is_nan() {
 							z_last_not_nan = z;
 						}
@@ -150,15 +149,18 @@ impl Color {
 const WHITE: Color = Color(0xffffff);
 const BLACK: Color = Color(0x000000);
 
-const RED   : Color = Color(0xff0000);
-const ORANGE: Color = Color(0xff8800);
-const YELLOW: Color = Color(0xffff00);
-const YELLEN: Color = Color(0x88ff00);
-const GREEN : Color = Color(0x00ff00);
-const GRYAN : Color = Color(0x00ff88);
-const CYAN  : Color = Color(0x00ffff);
-const BLYUAN: Color = Color(0x0088ff);
-const BLUE  : Color = Color(0x0000ff);
+const RED    : Color = Color(0xff0000);
+const ORANGE : Color = Color(0xff8800);
+const YELLOW : Color = Color(0xffff00);
+const YELLEN : Color = Color(0x88ff00);
+const GREEN  : Color = Color(0x00ff00);
+const GRYAN  : Color = Color(0x00ff88);
+const CYAN   : Color = Color(0x00ffff);
+const BLYUAN : Color = Color(0x0088ff);
+const BLUE   : Color = Color(0x0000ff);
+const BRED   : Color = Color(0x8800ff);
+const MAGENTA: Color = Color(0xff00ff);
+const REGENTA: Color = Color(0xff0088);
 
 
 
@@ -205,15 +207,18 @@ impl FloatToColor {
 			Self::Rainbow => {
 				multipoint_gradient_1d(t, &[
 					(-1.01, WHITE),
-					((0./8.)*2.-1., RED   ),
-					((1./8.)*2.-1., ORANGE),
-					((2./8.)*2.-1., YELLOW),
-					((3./8.)*2.-1., YELLEN),
-					((4./8.)*2.-1., GREEN ),
-					((5./8.)*2.-1., GRYAN ),
-					((6./8.)*2.-1., CYAN  ),
-					((7./8.)*2.-1., BLYUAN),
-					((8./8.)*2.-1., BLUE  ),
+					(( 0./11.)*2.-1., RED   ),
+					(( 1./11.)*2.-1., ORANGE),
+					(( 2./11.)*2.-1., YELLOW),
+					(( 3./11.)*2.-1., YELLEN),
+					(( 4./11.)*2.-1., GREEN ),
+					(( 5./11.)*2.-1., GRYAN ),
+					(( 6./11.)*2.-1., CYAN  ),
+					(( 7./11.)*2.-1., BLYUAN),
+					(( 8./11.)*2.-1., BLUE  ),
+					(( 9./11.)*2.-1., BRED   ),
+					((10./11.)*2.-1., MAGENTA),
+					((11./11.)*2.-1., REGENTA),
 					(1.01, WHITE),
 				])
 			}
@@ -223,18 +228,21 @@ impl FloatToColor {
 
 
 
-fn screen_to_world(
+struct STW {
 	x: float,
 	y: float,
-	screen_w: usize,
-	screen_h: usize,
+	/// screen_w
+	wf: float,
+	/// screen_h
+	hf: float,
 	zoom: float,
 	cam_x: float,
 	cam_y: float,
-) -> (float, float) {
-	let aspect_ratio = screen_h as float / screen_w as float;
-	let world_x = (x - screen_w as float / 2.0) / (screen_w as float * 0.5) / zoom + cam_x;
-	let world_y = (y - screen_h as float / 2.0) / (screen_h as float * 0.5) * aspect_ratio / zoom + cam_y;
+}
+fn screen_to_world(STW { x, y, wf, hf, zoom, cam_x, cam_y }: STW) -> (float, float) {
+	let aspect_ratio = hf / wf;
+	let world_x = cam_x + (x - wf * 0.5) / (wf * 0.5) / zoom;
+	let world_y = cam_y + (y - hf * 0.5) / (hf * 0.5) / zoom * aspect_ratio;
 	(world_x, world_y)
 }
 
